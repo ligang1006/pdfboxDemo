804e79c515c336c70419ec71c1b0b27a0771f635
ligangag <ligangag@yonyou.com>
2020-08-20 20:24:23 +0800

    文件地址修改

--- src/main/java/utils/DrawText.java
+++ src/main/java/utils/DrawText.java
@@ -4,11 +4,13 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.text.PDFTextStripperByArea;
 import org.apache.pdfbox.util.Matrix;
 
 import java.awt.*;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -23,27 +25,32 @@ public class DrawText {
         File file = new File(PATH);
         PDDocument document = PDDocument.load(file);
         //Retrieving a page of the PDF Document
-        PDPage page = document.getPage(1);
-        PDPageContentStream content = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, false);
-        PDFont font = PDType1Font.HELVETICA_BOLD;
-        int cursorX = 70;
-        int cursorY = 50;
-        //draw text
-        content.setNonStrokingColor(Color.BLACK);
-        content.beginText();
-        content.setFont(font, 20);
-//        Matrix matrix = new Matrix();
-//        content.setTextMatrix(matrix);
-//        content.setTextMatrix(cursorX,cursorY);
-        content.newLineAtOffset(cursorX + 10, cursorY + 10);
-        content.showText("sssssssssss");
-        content.endText();
-
-        //draw rectangle
-        content.setNonStrokingColor(Color.RED);
-        content.addRect(cursorX, cursorY, 70, 50);
-        content.close();
-        document.save(new File("/Users/ligang/Desktop/textOnBackground.pdf"));
+        for (int i = 0; i < 10; i++) {
+            PDPage page = document.getPage(i);
+            PDPageContentStream content = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND, false);
+            PDType0Font font = PDType0Font.load(document, new File("/Users/ligang/IdeaProjects/pdfboxDemo/src/main/resources/ygyjfcs.ttf"));
+//            PDFont font = PDType1Font.ZAPF_DINGBATS;
+            font.encode("utf8");
+            int cursorX = 70;
+            int cursorY = 50;
+            //draw rectangle
+//            content.setNonStrokingColor(Color.WHITE);
+//            content.addRect(cursorX, cursorY, 100, cursorY + 100);
+            content.fill();
+            content.saveGraphicsState();
+            //draw text
+            content.setNonStrokingColor(Color.BLACK);
+            content.beginText();
+            content.setFont(font, 20);
+            content.newLineAtOffset(cursorX + 10, cursorY + 100);
+            content.showText("撒打算的");
+            content.endText();
+            content.close();
+        }
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        document.save(byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        byteArrayOutputStream.close();
         document.close();
     }
 }
